a6bfec56917c64f518ca6bcffab3ae55c1009645
theophilousmapiye28 <theophilousmapiye28@gmail.com>
2024-08-22 17:24:15 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm:'480px', 
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+      },
+      
+    extend: {
+      colors:{
+brightRed:'hsl(12, 88%, 59%)',
+brightRedLight:'hsl(12, 88%,69%)',
+brightRedLight:'hsl(12, 88%, 95%)',
+darkBlue:'hsl(228, 39%,2 3%)',
+darkGrayishBlue:'hsl(227, 12%, 61%)',
+veryDarkBlue:'hsl(233, 12%, 13%)',
+veryPaleRed:'hsl(13, 100%, 96%)',
+veryLightGray:'hsl(0, 0%,98%)',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
